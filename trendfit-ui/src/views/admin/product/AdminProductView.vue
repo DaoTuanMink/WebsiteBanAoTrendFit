627599b04d3c2cfbebcaea6ee878be11570fab12
minhth03949 <minhdtth03949@fpt.edu.vn>
2026-06-20 00:05:36 +0700

    Fix import path for API_BASE in AdminProductView.vue

--- trendfit-ui/src/views/admin/product/AdminProductView.vue
+++ trendfit-ui/src/views/admin/product/AdminProductView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
-import API_BASE from '../../config/api.js'
+import API_BASE from '../../../config/api.js'
 
 const danhSachSanPham = ref([])
 const loading = ref(true)
